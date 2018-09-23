d66996ddbbfe8b2d63cc2d797dbf23913b1f137e
fandytic <fandy15ti@mahasiswa.pcr.ac.id>
2018-09-23 15:02:22 +0700

    update login
    design login fix

--- login.php
+++ login.php
@@ -1,44 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V15</title>
+	<title>Login Sistem Pakar</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="assetsL/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="assetsL/images/icons/favicon.png"/>
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="assetsL/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->	
 	<link rel="stylesheet" type="text/css" href="assetsL/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="assetsL/css/util.css">
 	<link rel="stylesheet" type="text/css" href="assetsL/css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
 <body>
 	
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100">
-				<div class="login100-form-title" style="background-image: url(assetsL/images/bg-01.jpg);">
+				<div class="login100-form-title" style="background-image: url(assetsL/images/bg-02.jpg);">
 					<span class="login100-form-title-1">
 						Sign In
 					</span>
 				</div>
 
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" action="">
 					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
 						<span class="label-input100">Username</span>
 						<input class="input100" type="text" name="username" placeholder="Enter username">
@@ -51,7 +51,16 @@
 						<span class="focus-input100"></span>
 					</div>
 
+					<div class="flex-sb-m w-full p-b-30">
+						<div class="contact100-form-checkbox">
+						</div>
 
+						<div>
+							<a href="#" class="txt1">
+								Kembali ke website
+							</a>
+						</div>
+					</div>
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
 							Login
@@ -62,21 +71,21 @@
 		</div>
 	</div>
 	
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/vendor/bootstrap/js/popper.js"></script>
 	<script src="assetsL/vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/vendor/daterangepicker/moment.min.js"></script>
 	<script src="assetsL/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="assetsL/js/main.js"></script>
 
 </body>
