f346038b962fa9a8670f7ceb7da92a44473f69e1
fandytic <fandy15ti@mahasiswa.pcr.ac.id>
2018-10-03 13:14:12 +0700

    update

--- controller/c_BasisP.php
+++ controller/c_BasisP.php
@@ -41,7 +41,7 @@ class BasisP
 	function TampilRelasi()
 	{
 		include "../koneksi/koneksi.php";
-		$query = mysqli_query($con, "SELECT a.*, g.nama as nama_gejala,p.nama as nama_penyakit from ds_aturan a,ds_gejala g, ds_penyakit p where a.id_penyakit=p.id and a.id_gejala = p.id");
+		$query = mysqli_query($con, "SELECT a.*, g.nama as nama_gejala,p.nama as nama_penyakit from ds_aturan a,ds_gejala g, ds_penyakit p where a.id_penyakit=p.id and a.id_gejala = g.id");
 
 		//$query = mysqli_query($con, "SELECT * from ds_aturan a, ds_penyakit b, ds_gejala c where b.id = a.id_penyakit");
 
