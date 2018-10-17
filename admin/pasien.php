798efb89346ac779d4184e6c6e89c894e7289522
fandytic <fandy15ti@mahasiswa.pcr.ac.id>
2018-10-17 13:49:51 +0700

    update halaman admin

--- admin/pasien.php
+++ admin/pasien.php
@@ -10,11 +10,11 @@
     <div class="page-breadcrumb">
         <div class="row align-items-center">
             <div class="col-5">
-                <h4 class="page-title">Manajemen Rekam Medis</h4>
+                <h4 class="page-title">Manajemen Pasien</h4>
                 <div class="d-flex align-items-center">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Rekam Medis</li>
+                        <li class="breadcrumb-item active" aria-current="page">Pasien</li>
                     </ol>
                 </div>
             </div>
--- admin/riwayatd.php
+++ admin/riwayatd.php
@@ -1,85 +1,84 @@
-<?php include '_header.php'; ?>
+<?php include '_header.php'; 
+?>
+<!-- ============================================================== -->
+<!-- Page wrapper  -->
+<!-- ============================================================== -->
+<div class="page-wrapper">
     <!-- ============================================================== -->
-    <!-- Page wrapper  -->
+    <!-- Bread crumb and right sidebar toggle -->
     <!-- ============================================================== -->
-    <div class="page-wrapper">
-        <!-- ============================================================== -->
-        <!-- Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <div class="page-breadcrumb">
-            <div class="row align-items-center">
-                <div class="col-5">
-                    <h4 class="page-title">Riwayat Diagnosa</h4>
-                    <div class="d-flex align-items-center">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Riwayat Diagnosa</li>
-                        </ol>
-                    </div>
-                </div>
-                <div class="col-7">
-                    <div class="text-right upgrade-btn">
-                    </div>
+    <div class="page-breadcrumb">
+        <div class="row align-items-center">
+            <div class="col-5">
+                <h4 class="page-title">Riwayat Diagnosa</h4>
+                <div class="d-flex align-items-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Riwayat Diagnosa</li>
+                    </ol>
                 </div>
             </div>
-        </div>
-        <!-- ============================================================== -->
-        <!-- End Bread crumb and right sidebar toggle -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Container fluid  -->
-        <!-- ============================================================== -->
-        <div class="container-fluid">
-            <!-- ============================================================== -->
-            <!-- Start Page Content -->
-            <!-- ============================================================== -->
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table id="bootstrap-data-table" class="table table-striped table-bordered">
-                                    <thead>
-                                      <tr>
-                                        <th>Nama Pasien</th>
-                                        <th>Umur</th>
-                                        <th>Gejala yang dipilih</th>
-                                        <th>Hasil Diagnosa</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td>G1</td>
-                                    <td>System Architect</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <td>G2</td>
-                                    <td>Accountant</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+            <div class="col-7">
+                <div class="text-right upgrade-btn">
+                    <a href="tpenyakit.php" class="btn btn-danger text-white"><i class="mdi mdi-plus"></i> Tambah Data</a>
                 </div>
             </div>
         </div>
     </div>
     <!-- ============================================================== -->
-    <!-- End PAge Content -->
+    <!-- End Bread crumb and right sidebar toggle -->
     <!-- ============================================================== -->
     <!-- ============================================================== -->
-    <!-- Right sidebar -->
+    <!-- Container fluid  -->
     <!-- ============================================================== -->
-    <!-- .right-sidebar -->
-    <!-- ============================================================== -->
-    <!-- End Right sidebar -->
-    <!-- ============================================================== -->
+    <div class="container-fluid">
+        <!-- ============================================================== -->
+        <!-- Start Page Content -->
+        <!-- ============================================================== -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table id="bootstrap-data-table" class="table table-hover table-bordered">
+                                <thead style="background-color: #7f8c8d; color: white;">
+                                  <tr>
+                                    <th style="color: white;" width="13%">Kode Penyakit</th>
+                                    <th style="color: white;">Nama Penyakit</th>
+                                    <th style="color: white;">Keterangan Penyakit</th>
+                                    <th style="color: white;">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td>
+                                            <a href="epenyakit.php?id=<?php print $d['id']; ?>" class="btn btn-info btn-simple btn-xs text-white" title="Edit Penyakit"><i class="mdi mdi-lead-pencil"></i></a>
+
+                                            <a href="../ProsesA/d_penyakit.php?id=<?php print $d['id']; ?>" class="btn btn-danger btn-simple btn-xs text-white" title="Hapus Penyakit"><i class="fa fa-times"></i></a>
+                                        </td>
+                                    </tr>
+                            </tbody>
+                        </table>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- ============================================================== -->
+<!-- End PAge Content -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- Right sidebar -->
+<!-- ============================================================== -->
+<!-- .right-sidebar -->
+<!-- ============================================================== -->
+<!-- End Right sidebar -->
+<!-- ============================================================== -->
 </div>
 <!-- ============================================================== -->
 <!-- End Container fluid  -->
