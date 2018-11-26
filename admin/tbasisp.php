74de8b36aff574e9a00b3efa1704fffb94595c6b
fandytic <fandy15ti@mahasiswa.pcr.ac.id>
2018-11-26 23:23:43 +0700

    tes jquery
    sukses, belum termasuk ajax

--- admin/tbasisp.php
+++ admin/tbasisp.php
@@ -41,71 +41,110 @@ $data2 = $tt->TampilSemua();
 		<!-- ============================================================== -->
 		<div class="row">
 			<!-- Column -->
-			<div class="col-lg-8 col-xlg-9 col-md-7">
+			<div class="col-12">
 				<div class="card">
 					<div class="card-body">
-						<form id="myform" method="post" class="form-horizontal form-material" action="../ProsesA/t_basisp.php">
-							<div class="form-group">
-								<label class="col-md-12">Nama Penyakit</label>
-								<div class="col-md-12">
-									<select class="form-control form-control-line" name="id_penyakit">
-										<?php foreach($data as $d){
-											?>
-											<option value="<?php print $d['id']; ?>"><?php print $d['nama']; ?></option>
-										<?php } ?>
-									</select>
+						<!-- <form id="myform" method="post" class="form-horizontal form-material" action="../ProsesA/t_basisp.php"> -->
+							<form name="add_name" id="add_name">
+								<div class="form-group">
+									<table class="table table-bordered" id="dynamic_field">
+										<tr>
+											<td>Tingkatan Penyakit :</td>
+											<td>
+												<select class="form-control form-control-line" name="id_penyakit">
+													<?php foreach($data as $d){
+														?>
+														<option value="<?php print $d['id']; ?>"><?php print $d['nama']; ?></option>
+													<?php } ?>
+												</select>
+											</td>
+										</tr>
+										<tr>
+											<td>Gejala : </td>
+											<td>
+												<select class="form-control form-control-line" name="id_gejala">
+													<?php foreach($data2 as $dd){
+														?>
+														<option value="<?php print $dd['id']; ?>"><?php print $dd['nama']; ?></option>
+													<?php } ?>
+												</select>
+											</td>
+											<td>
+												<input id="numb" type="text" class="form-control form-control-line" name="ds" placeholder="contoh input nilai : 0.5">
+											</td>
+											<td>
+												
+											</td>
+										</tr>
+									</table>
+									<div class="form-group">
+										<div class="col-sm-12">
+											<button name="add" id="add" class="btn btn-failed" type="button">Tambah Gejala</button>
+											<button class="btn btn-success" type="button">Submit</button>
+										</div>
+									</div>
 								</div>
-							</div>
-							<div class="form-group">
-								<label class="col-md-12">Nama Gejala</label>
-								<div class="col-md-12">
-									<select class="form-control form-control-line" name="id_gejala">
-										<?php foreach($data2 as $dd){
-											?>
-											<option value="<?php print $dd['id']; ?>"><?php print $dd['nama']; ?></option>
-										<?php } ?>
-									</select>
+								<!-- <div class="form-group">
+									<label class="col-md-12">Nama Penyakit</label>
+									<div class="col-md-12">
+										<select class="form-control form-control-line" name="id_penyakit">
+											<?php foreach($data as $d){
+												?>
+												<option value="<?php print $d['id']; ?>"><?php print $d['nama']; ?></option>
+											<?php } ?>
+										</select>
+									</div>
 								</div>
-							</div>
+								<div class="form-group">
+									<label class="col-md-12">Nama Gejala</label>
+									<div class="col-md-12">
+										<select class="form-control form-control-line" name="id_gejala">
+											<?php foreach($data2 as $dd){
+												?>
+												<option value="<?php print $dd['id']; ?>"><?php print $dd['nama']; ?></option>
+											<?php } ?>
+										</select>
+									</div>
+								</div>
 
-							<div class="form-group">
-								<label class="col-md-12">Nilai Kepercayaan</label>
-								<div class="col-md-12">
-									<input id="numb" type="text" class="form-control form-control-line" name="ds" placeholder="contoh input nilai : 0.5">
-									<p id="demo" style="color: red;"></p>
+								<div class="form-group">
+									<label class="col-md-12">Nilai Kepercayaan</label>
+									<div class="col-md-12">
+										<input id="numb" type="text" class="form-control form-control-line" name="ds" placeholder="contoh input nilai : 0.5">
+										<p id="demo" style="color: red;"></p>
+									</div>
 								</div>
-							</div>
 
-							<div class="form-group">
-								<div class="col-sm-12">
-									<button onclick="myFunction()" class="btn btn-success" type="button">Tambah Data</button>
-								</div>
-							</div>
-						</form>
+								<div class="form-group">
+									<div class="col-sm-12">
+										<button onclick="myFunction()" class="btn btn-success" type="button">Tambah Data</button>
+									</div>
+								</div> -->
+							</form>
+						</div>
 					</div>
 				</div>
+				<!-- Column -->
 			</div>
-			<!-- Column -->
 		</div>
 	</div>
-</div>
-<!-- ============================================================== -->
-<!-- End PAge Content -->
-<!-- ============================================================== -->
-<!-- ============================================================== -->
-<!-- Right sidebar -->
-<!-- ============================================================== -->
-<!-- .right-sidebar -->
-<!-- ============================================================== -->
-<!-- End Right sidebar -->
-<!-- ============================================================== -->
+	<!-- ============================================================== -->
+	<!-- End PAge Content -->
+	<!-- ============================================================== -->
+	<!-- ============================================================== -->
+	<!-- Right sidebar -->
+	<!-- ============================================================== -->
+	<!-- .right-sidebar -->
+	<!-- ============================================================== -->
+	<!-- End Right sidebar -->
+	<!-- ============================================================== -->
 </div>
 <!-- ============================================================== -->
 <!-- End Container fluid  -->
 <!-- ============================================================== -->
 <?php include '_footer.php'; ?>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 	function myFunction(){
 		var x, text;
 		// Get the value of the input field with id="numb"
@@ -116,8 +155,24 @@ $data2 = $tt->TampilSemua();
     	text = "*Format yang diinputkan harus antara 0 - 1";
     } else {
     	text = "Format benar";
-		document.forms["myform"].submit();
+    	document.forms["myform"].submit();
     }
     document.getElementById("demo").innerHTML = text;
 }
+</script> -->
+<script>
+	$(document).ready(function(){
+		var i = 1;
+		$('#add').click(function(){
+			i++;
+			$('#dynamic_field').append('<tr id="row'+i+'"><td></td><td><select class="form-control form-control-line" name="id_gejala">													<?php foreach($data2 as $dd){
+				?>
+				<option value="<?php print $dd['id']; ?>"><?php print $dd['nama']; ?></option>													<?php } ?>
+				</select></td><td>												<input id="numb" type="text" class="form-control form-control-line" name="ds" placeholder="contoh input nilai : 0.5">											</td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>')
+		});
+		$(document).on('click','.btn_remove', function(){
+			var button_id = $(this).attr("id");
+			$('#row'+button_id+'').remove();
+		});
+	});
 </script>
